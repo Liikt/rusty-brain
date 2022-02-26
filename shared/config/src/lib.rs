d7fec7bb89b67f118ef76ae0918324150f4a6f46
Julian Beier <jb@futureplay.de>
2022-02-26 20:00:29 +0100

    add the compatibility_weight_coefficient

--- shared/config/src/lib.rs
+++ shared/config/src/lib.rs
@@ -3,14 +3,20 @@ pub struct Config {
     innovation: u64,
     aggregate_mutation_chance: f64,
     activation_mutation_chance: f64,
+    compatibility_weight_coefficient: f64
 }
 
 impl Config {
-    pub fn new(agg_chance: f64, act_chance: f64) -> Self {
+    pub fn new(
+        agg_chance: f64, 
+        act_chance: f64,
+        comp_weight_coeff: f64
+    ) -> Self {
         Config { 
             innovation: 0,
             aggregate_mutation_chance: agg_chance,
-            activation_mutation_chance: act_chance
+            activation_mutation_chance: act_chance,
+            compatibility_weight_coefficient: comp_weight_coeff
         }
     }
 
@@ -27,5 +33,9 @@ impl Config {
 
     pub fn act_mut_chance(&self) -> f64 {
         self.activation_mutation_chance
+    }
+
+    pub fn compat_weight_coeff(&self) -> f64 {
+        self.compatibility_weight_coefficient
     }
 }
