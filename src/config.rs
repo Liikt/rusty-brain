56e67843e9c80b2bfd043fbacc4cc44b4cb3d5a4
Julian Beier <jb@futureplay.de>
2022-02-26 17:57:33 +0100

    aggregate and activate function chances in config

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,30 @@
 pub struct Config {
-    innovation: u64
+    innovation: u64,
+    aggregate_mutation_chance: f64,
+    activation_mutation_chance: f64,
 }
 
 impl Config {
     pub fn new() -> Self {
-        Config { innovation: 0 }
+        Config { 
+            innovation: 0,
+            aggregate_mutation_chance: 0.2,
+            activation_mutation_chance: 0.2
+        }
     }
 
     pub fn new_innovation(&mut self) -> u64 {
         let ret = self.innovation;
         self.innovation = self.innovation.checked_add(1)
-            .unwrap("No innovations available anymore");
+            .expect("No innovations available anymore");
         ret
+    }
+
+    pub fn agg_mut_chance(&self) -> f64 {
+        self.aggregate_mutation_chance
+    }
+
+    pub fn act_mut_chance(&self) -> f64 {
+        self.activation_mutation_chance
     }
 }
