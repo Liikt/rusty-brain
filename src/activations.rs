6a97fa0b99c1ca0fcb66372d127043a34a2aa7d6
Julian Beier <jb@futureplay.de>
2022-02-26 17:56:58 +0100

    change activation functions so that they can be used correctly

--- src/activations.rs
+++ src/activations.rs
@@ -2,81 +2,111 @@
 pub struct ActivationFunctions;
 
 impl ActivationFunctions {
-    pub fn sigmoid(val: f64) -> f64 {
+    fn sigmoid(val: f64) -> f64 {
         let tmp = -60.0_f64.max(60.0_f64.min(5.0 * val));
         1.0 / (1.0 + (-tmp).exp())
     }
 
-    pub fn tanh(val: f64) -> f64 {
+    fn tanh(val: f64) -> f64 {
         -60.0_f64.max(60.0_f64.min(2.5 * val)).tanh()
     }
 
-    pub fn sin(val: f64) -> f64 {
+    fn sin(val: f64) -> f64 {
         -60.0_f64.max(60.0_f64.min(5.0 * val)).sin()
     }
 
-    pub fn gauss(val: f64) -> f64 {
+    fn gauss(val: f64) -> f64 {
         let tmp = -3.4_f64.max(3.4_f64.min(val));
         (-5.0 * tmp * tmp).exp()
     }
 
-    pub fn relu(val: f64) -> f64 {
+    fn relu(val: f64) -> f64 {
         val.max(0.0)
     }
 
-    pub fn elu(val: f64) -> f64 {
+    fn elu(val: f64) -> f64 {
         if val > 0.0 { val } else { val.exp_m1() }
     }
 
-    pub fn lelu(val: f64) -> f64 {
+    fn lelu(val: f64) -> f64 {
         if val > 0.0 { val } else { val * 0.005 }
     }
 
-    pub fn selu(val: f64) -> f64 {
+    fn selu(val: f64) -> f64 {
         let lam = 1.0507009873554805;
         let alpha = 1.6732632423543772;
         if val > 0.0 { lam * val } else { lam * alpha * (val.exp_m1()) }
     }
 
-    pub fn softplus(val: f64) -> f64 {
+    fn softplus(val: f64) -> f64 {
         let tmp = -60.0_f64.max(60.0_f64.min(5.0 * val));
         0.2 * tmp.exp().ln_1p()
     }
 
-    pub fn identity(val: f64) -> f64 {
+    fn identity(val: f64) -> f64 {
         val
     }
 
-    pub fn clamped(val: f64) -> f64 {
+    fn clamped(val: f64) -> f64 {
         val.clamp(-1.0, 1.0)
     }
 
-    pub fn inv(val: f64) -> f64 {
+    fn inv(val: f64) -> f64 {
         let tmp = 1.0 / val;
         if tmp.is_infinite() { val } else { tmp }
     }
 
-    pub fn log(val: f64) -> f64 {
+    fn log(val: f64) -> f64 {
         val.max(1.0_f64.powi(-7)).ln()
     }
 
-    pub fn exp(val: f64) -> f64 {
+    fn exp(val: f64) -> f64 {
         -60.0_f64.max(60.0_f64.min(5.0 * val)).exp()
     }
 
-    pub fn abs(val: f64) -> f64 {
+    fn abs(val: f64) -> f64 {
         val.abs()
     }
 
-    pub fn hat(val: f64) -> f64 {
+    fn hat(val: f64) -> f64 {
         0.0_f64.max(1.0 - val)
     }
 
-    pub fn square(val: f64) -> f64 {
+    fn square(val: f64) -> f64 {
         val * val
     }
 
-    pub fn cube(val: f64) -> f64 {
+    fn cube(val: f64) -> f64 {
         val * val * val
+    }
+
+    pub fn get_num_funcs() -> u8 {
+        18
+    }
+
+    pub fn activate(fid: u8, val: f64) -> f64 {
+        match fid {
+            0  => Self::sigmoid(val),
+            1  => Self::tanh(val),
+            2  => Self::sin(val),
+            3  => Self::gauss(val),
+            4  => Self::relu(val),
+            5  => Self::elu(val),
+            6  => Self::lelu(val),
+            7  => Self::selu(val),
+            8  => Self::softplus(val),
+            9  => Self::identity(val),
+            10 => Self::clamped(val),
+            11 => Self::inv(val),
+            12 => Self::log(val),
+            13 => Self::exp(val),
+            14 => Self::abs(val),
+            15 => Self::hat(val),
+            16 => Self::square(val),
+            17 => Self::cube(val),
+
+            _ => panic!("Max Function ID: {} Got: {}",
+                Self::get_num_funcs(), fid)
+        }
     }
 }
