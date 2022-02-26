7cfc222bee7771774968ad12b918255d39b4cd08
Julian Beier <jb@futureplay.de>
2022-02-26 20:02:26 +0100

    implementation of a standard connection gene

--- nnn/src/gene.rs
+++ nnn/src/gene.rs
@@ -4,7 +4,7 @@ use std::cell::RefCell;
 use rand::random;
 
 use config::Config;
-use gene::{NodeType, NodeGene};
+use gene::{NodeType, NodeGene, ConnectionGene};
 
 use crate::activations::ActivationFunctions;
 use crate::aggregations::AggregationFunctions;
@@ -70,6 +70,51 @@ impl NodeGene for StandardNode {
             tmp += 1.0;
         }
 
+        tmp * weight_coeff
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct StandardConnection {
+    src: u64,
+    dst: u64,
+    weight: f64,
+    enabled: bool,
+    innovation: u64
+}
+
+impl StandardConnection {
+    pub fn new(src: u64, dst: u64, innovation: u64) -> Self {
+        StandardConnection { src, dst, weight: 1.0, enabled: true, innovation }
+    }
+}
+
+impl ConnectionGene for StandardConnection {
+    fn get_src(&self) -> u64 {
+        self.src
+    }
+
+    fn get_dst(&self) -> u64 {
+        self.dst
+    }
+
+    fn get_weight(&self) -> f64 {
+        self.weight
+    }
+
+    fn get_innovation(&self) -> u64 {
+        self.innovation
+    }
+
+    fn is_enabled(&self) -> bool {
+        self.enabled
+    }
+
+    fn distance(&self, other: &Self, weight_coeff: f64) -> f64 {
+        let mut tmp = (self.weight - other.weight).abs();
+        if self.enabled != other.enabled {
+            tmp += 1.0
+        }
         tmp * weight_coeff
     }
 }
